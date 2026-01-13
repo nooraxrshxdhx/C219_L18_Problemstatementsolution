a156535fd0fd0327d5ef3e90b88a733c5eeb54ce
nooraxrshxdhx <24020989@myrp.edu.sg>
2026-01-13 23:52:04 +0800

    Completed React Router course project

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import logo from "../assets/red30-tech-logo.png";
+import {Routes, Route} from "react-router-dom";
 
+import Home from "./Home";
+import About from "./About";
+import Categories from "./Categories";
+import Header from "./Header";
+import Category from "./Category";
+import Session from "./Session";
+import Register from "./Register";
+import Confirmation from "./Confirmation";
 function App() {
   return (
     <div className="app">
-      <header className="container">
-        <img
-          className="logo"
-          src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
-        />
-      </header>
+
+      <Header />
+
+      
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech"/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="categories" element={<Categories />}>
+           <Route path=":catId" element={<Category/>} >
+           <Route path=":sessionId" element={<Session/>}/>
+           </Route>
+           <Route index element={<h3>Select a category from above</h3>}/>
+        </Route> 
+        <Route path="register" element={<Register/>} />
+        <Route path="confirmed" element={<Confirmation/>} />
+
+        <Route path="*" element={<h1 className="not-found">Page not found</h1>} />
+
+
+
+      </Routes>
+
+
+
+
 
       <footer className="container">
         &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,26 @@
+import {getCategories} from "../api";
+import { Outlet , NavLink} from "react-router-dom";
+
+
 export default function Categories() {
+  const categories = getCategories();
+
   return (
     <div className="container">
       <h1>Session Categories</h1>
+    
+      <ul className="categories">
+        {categories.map(cat => 
+        <li key={cat.id}>
+          <NavLink 
+            className={({isActive}) => isActive ? "category-active" : null }
+            to={cat.id}
+          >
+            {cat.name}
+          </NavLink>
+          </li>)}
+      </ul>
+      <Outlet />
     </div>
   );
 }
